9208a5d4d3573422e190021c9046311f4a2c52e5
Nick <13785390+quizque@users.noreply.github.com>
2023-05-02 17:07:17 -0400

    Tank Testing

--- src/main/java/frc/robot/kernal/BuildConstants.java
+++ src/main/java/frc/robot/kernal/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-5885-Template";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 9;
-  public static final String GIT_SHA = "7686e524ccbe626a0f69e993c84331abe45286ed";
-  public static final String GIT_DATE = "2023-04-27 19:23:42 EDT";
+  public static final int GIT_REVISION = 10;
+  public static final String GIT_SHA = "43c6a7be8ada7d8c5c8ed5c25ced1eba88a27bd9";
+  public static final String GIT_DATE = "2023-05-02 16:49:35 EDT";
   public static final String GIT_BRANCH = "Swerve-sim";
-  public static final String BUILD_DATE = "2023-04-27 20:09:52 EDT";
-  public static final long BUILD_UNIX_TIME = 1682640592521L;
+  public static final String BUILD_DATE = "2023-05-02 17:05:31 EDT";
+  public static final long BUILD_UNIX_TIME = 1683061531324L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
